7f60234603cc91b1a102c4850ac5650c46d3c676
manojskanth <manojskanth@gmail.com>
2026-06-23 10:07:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,7 @@ FACULTY_DIRECTORY = {
     "ismail@stmaryscollege.in": {"name": "Mr. Ismail C", "secret_key": "ismail_pass"},
     "aksharasingh@stmaryscollege.in": {"name": "Dr. Akshara Singh", "secret_key": "akshara_pass"},
     "vasantharao@stmaryscollege.in": {"name": "Mr. Vasantha Rao B", "secret_key": "vasantharao_pass"}
+    "gisageorge@stmaryscollege.in": {"name": "Ms. Gisa George", "secret_key": "gisageorge_pass"}
 }
 
 # --- 2. GOOGLE SERVICE INTEGRATION HANDSHAKE ---
